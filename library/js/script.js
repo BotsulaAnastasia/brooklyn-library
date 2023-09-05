4782136c5f90f0e49947db2926e7c333f789fc36
Anastasia Botsula <nactya.botsula@gmail.com>
2023-09-06 01:14:01 +0300

    feat: add slider to about section

--- library/js/script.js
+++ library/js/script.js
@@ -1,12 +1,12 @@
 var burgerMenu = document.querySelector('.tablet-menu');
 var menuBody = document.querySelector('.header-navigation');
-if (burgerMenu){
+if (burgerMenu) {
     burgerMenu.addEventListener("click", function (e) {
         burgerMenu.classList.toggle('--active');
         menuBody.classList.toggle('--active');
     })
 }
-
+/*
 if (menuBody.classList.contains('--active')) {
     const menuBody = document.querySelector('.header-navigation');
      
@@ -17,3 +17,37 @@ if (menuBody.classList.contains('--active')) {
     };
 
 }
+
+const carretLeft = document.querySelector('#carret-left');
+const carretRight = document.querySelector('#carret-right');
+
+const sliderImg = document.querySelector('#slider-images');
+
+carretRight.addEventListener("click", () => {
+    sliderImg.classList.add('transition-right');
+})
+*/
+
+let carretLeft = document.getElementById("carret-left"),
+    carretRight = document.getElementById("carret-right"),
+    sliderImg = document.getElementById("slider-images"),
+    left = 0,
+    imgWidth = 475;
+
+sliderImg.style.left = left + 'px';
+
+slideTo = (direction) => {
+    if (direction === 'right') left -= imgWidth;
+    if (direction === 'left') left += imgWidth;
+    if (left >= 0) left = 0;
+    if (left < -1900) left = -1900;  
+    sliderImg.style.left = left + 'px';
+}
+
+carretLeft.addEventListener("click", () => {
+    slideTo('left');
+});
+
+carretRight.addEventListener("click", () => {
+    slideTo('right');
+});
